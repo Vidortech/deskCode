1c7743d351d478e205603c526fb33175fbcea8a4
Matchzin <matheusvidordeoliveira@gmail.com>
2024-04-24 07:55:45 -0300

    Commit atualizações finais

--- funcoes.py
+++ funcoes.py
@@ -1,64 +1,53 @@
-import ast
 import math
 
 
-
-
-def calcular_taxa_erro_software(erros, linhas_codigo):
+def calcular_velocidade_media(distancia, tempo):
     try:
-        erros = float(erros)
-        linhas_codigo = float(linhas_codigo)
-        return (erros / linhas_codigo) * 100
+        distancia = float(distancia)
+        tempo = float(tempo)
+        return distancia / tempo
     except ValueError:
         return None
 
+def calcular_forca_massa_aceleracao(massa, aceleracao):
+    try:
+        massa = float(massa)
+        aceleracao = float(aceleracao)
+        return massa * aceleracao
+    except ValueError:
+        return None
 
-
-# Fórmula para cálculo de Bháskara
 def bhaskara(a, b, c):
     try:
         a = float(a)
         b = float(b)
         c = float(c)
-
-        delta = b**2 - 4 * a * c
-
+        delta = b ** 2 - 4 * a * c
         if delta < 0:
-            return 'Não existem raízes reais.'
+            return "Não há raízes reais."
         elif delta == 0:
-            root = -b / (2 * a)
-            return f'Existe apenas uma raiz real: {root:.2f}'
+            x = -b / (2 * a)
+            return f'A única raiz é {x:.2f}.'
         else:
-            root1 = (-b + math.sqrt(delta)) / (2 * a)
-            root2 = (-b - math.sqrt(delta)) / (2 * a)
-            return f'As raízes são: {root1:.2f} e {root2:.2f}'  
+            x1 = (-b + math.sqrt(delta)) / (2 * a)
+            x2 = (-b - math.sqrt(delta)) / (2 * a)
+            return f'As raízes são {x1:.2f} e {x2:.2f}.'
     except ValueError:
-        return 'Os coeficientes devem ser números.'
+        return "Por favor, insira valores numéricos."
 
-
-# Fórmula para cálculo de log Simples
-def logaritmo(num):
-    math.log(num)
-
-
-
-
-# Fórmula para cálculo de velocidade média.
-def calcular_velocidade_media(distancia, tempo):
+def calcular_taxa_erro_software(num_erros, linhas_codigo):
     try:
-        distancia = float(distancia)
-        tempo = float(tempo)
-        velocidade_media = distancia / tempo
-        return velocidade_media
+        num_erros = float(num_erros)
+        linhas_codigo = float(linhas_codigo)
+        return (num_erros / linhas_codigo) * 100
     except ValueError:
         return None
 
-# Fórmula para cálculo de força
-def calcular_forca_massa_aceleracao(massa, aceleracao):
+def calcular_equacao_movimento(velocidade_inicial, aceleracao, tempo):
     try:
-        massa = float(massa)
+        velocidade_inicial = float(velocidade_inicial)
         aceleracao = float(aceleracao)
-        forca = massa * aceleracao
-        return forca
+        tempo = float(tempo)
+        return velocidade_inicial * tempo + 0.5 * aceleracao * tempo ** 2
     except ValueError:
         return None
--- main.py
+++ main.py
@@ -4,17 +4,19 @@ from kivy.uix.label import Label
 from kivy.uix.popup import Popup
 from kivy.uix.button import Button
 from kivy.graphics import RoundedRectangle
+from kivy.uix.scrollview import ScrollView
 from kivy.properties import ListProperty
 from kivy.uix.textinput import TextInput
 from funcoes import *
+import math
 
 COR_PRINCIPAL = (0.18, 0.33, 0.52, 1)
 COR_SECUNDARIA = (0.96, 0.76, 0.32, 1)
 COR_FUNDO = (0.9, 0.9, 0.9, 1)
-COR_ROXA = (128/255, 0/255, 128/255, 1)  # Cor roxa
+COR_ROXA = (16/255, 127/255, 223/255, 1)  
 
 class RoundedButton(Button):
-    background_color = ListProperty(COR_ROXA)  # Alterando a cor de fundo para roxa
+    background_color = ListProperty(COR_ROXA)  
 
     def __init__(self, **kwargs):
         super(RoundedButton, self).__init__(**kwargs)
@@ -28,9 +30,9 @@ class RoundedButton(Button):
 
 class CalculadoraApp(App):
     def build(self):
-        layout = BoxLayout(orientation='vertical', padding=10, spacing=0)
-        titulo = Label(text="Calculadora de Física e Engenharia",
-                       font_size=20, color=COR_PRINCIPAL)
+        layout = BoxLayout(orientation='vertical', padding=20, spacing=2)
+        titulo = Label(text="Calculadora Moderna", font_size=36, color='#107fdf')
+        
         layout.add_widget(titulo)
 
         button_velocidade_media = RoundedButton(
@@ -69,12 +71,46 @@ class CalculadoraApp(App):
         button_seer.font_size = 18
         layout.add_widget(button_seer)
 
-        return layout
+        button_seno = RoundedButton(
+            text="Calcular Seno", size_hint_y=None, height=60)
+        button_seno.bind(on_press=self.mostrar_popup_seno)
+        button_seno.font_name = "Arial"
+        button_seno.font_size = 18
+        layout.add_widget(button_seno)
 
+        button_cosseno = RoundedButton(
+            text="Calcular Cosseno", size_hint_y=None, height=60)
+        button_cosseno.bind(on_press=self.mostrar_popup_cosseno)
+        button_cosseno.font_name = "Arial"
+        button_cosseno.font_size = 18
+        layout.add_widget(button_cosseno)
 
+        button_exponencial = RoundedButton(
+            text="Calcular Exponencial", size_hint_y=None, height=60)
+        button_exponencial.bind(on_press=self.mostrar_popup_exponencial)
+        button_exponencial.font_name = "Arial"
+        button_exponencial.font_size = 18
+        layout.add_widget(button_exponencial)
 
+        button_log_natural = RoundedButton(
+            text="Calcular Log Natural", size_hint_y=None, height=60)
+        button_log_natural.bind(on_press=self.mostrar_popup_log_natural)
+        button_log_natural.font_name = "Arial"
+        button_log_natural.font_size = 18
+        layout.add_widget(button_log_natural)
 
+        button_equacao_movimento = RoundedButton(
+            text="Calcular Equação de Movimento", size_hint_y=None, height=60)
+        button_equacao_movimento.bind(on_press=self.mostrar_popup_equacao_movimento)
+        button_equacao_movimento.font_name = "Arial"
+        button_equacao_movimento.font_size = 18
+        layout.add_widget(button_equacao_movimento)
 
+        scrollview = ScrollView()
+        scrollview.add_widget(layout)
+
+        return scrollview
+        
     def mostrar_popup_velocidade_media(self, instance):
         content = BoxLayout(orientation='vertical', padding=10, spacing=5)
         content.add_widget(Label(text="Distância (m):", color=COR_PRINCIPAL))
@@ -106,7 +142,7 @@ class CalculadoraApp(App):
             popup_resultado.open()
         else:
             popup_erro = Popup(title='Erro', content=Label(
-                text='Por favor, insira valores numéricos para distância e tempo.', color=COR_PRINCIPAL), size_hint=(None, None), size=(400, 200))
+                text='Por favor, insira valores numéricos.', color=COR_PRINCIPAL), size_hint=(None, None), size=(400, 200))
             popup_erro.open()
 
     def mostrar_popup_forca(self, instance):
@@ -142,10 +178,8 @@ class CalculadoraApp(App):
             popup_resultado.open()
         else:
             popup_erro = Popup(title='Erro', content=Label(
-                text='Por favor, insira valores numéricos para massa e aceleração.', color=COR_PRINCIPAL), size_hint=(None, None), size=(400, 200))
+                text='Por favor, insira valores numéricos.', color=COR_PRINCIPAL), size_hint=(None, None), size=(400, 200))
             popup_erro.open()
-
-
 
     def mostrar_popup_bhaskara(self, instance):
         content = BoxLayout(orientation='vertical', padding=10, spacing=5)
@@ -172,8 +206,6 @@ class CalculadoraApp(App):
                         content=content, size_hint=(None, None), size=(400, 400))
         popup.open()
 
-
-
     def calcular_bhaskara_callback(self, instance):
         coef_a = self.coef_a_input.text
         coef_b = self.coef_b_input.text
@@ -184,8 +216,6 @@ class CalculadoraApp(App):
         popup_resultado = Popup(title='Resultado', content=Label(
             text=resultado, color=COR_PRINCIPAL), size_hint=(None, None), size=(400, 200))
         popup_resultado.open()
-
-
 
     def mostrar_popup_logaritmo(self, instance):
         content = BoxLayout(orientation='vertical', padding=10, spacing=5)
@@ -201,26 +231,21 @@ class CalculadoraApp(App):
         content.add_widget(button_calcular)
 
         popup = Popup(title='Calcular Logaritmo',
-                    content=content, size_hint=(None, None), size=(400, 200))
+                    content=content, size_hint=(None, None), size=(400, 230))
         popup.open()
 
     def calcular_logaritmo_callback(self, instance):
         num = self.num_input.text
         try:
             num = float(num)
-            resultado = logaritmo(num)
+            resultado = math.log10(num)
             popup_resultado = Popup(title='Resultado', content=Label(
-                text=f'O logaritmo de {num} é {resultado}', color=COR_PRINCIPAL), size_hint=(None, None), size=(400, 200))
+                text=f'O logaritmo de {num} é {resultado:.2f}', color=COR_PRINCIPAL), size_hint=(None, None), size=(400, 200))
             popup_resultado.open()
         except ValueError:
             popup_erro = Popup(title='Erro', content=Label(
                 text='Por favor, insira um número válido.', color=COR_PRINCIPAL), size_hint=(None, None), size=(400, 200))
             popup_erro.open()
-
-
-
-
-
 
     def mostrar_popup_seer(self, instance):
         content = BoxLayout(orientation='vertical', padding=10, spacing=5)
@@ -254,8 +279,176 @@ class CalculadoraApp(App):
             popup_resultado.open()
         else:
             popup_erro = Popup(title='Erro', content=Label(
-                text='Por favor, insira valores numéricos para o número de erros e linhas de código.', color=COR_PRINCIPAL), size_hint=(None, None), size=(400, 200))
+                text='Por favor, insira valores numéricos.', color=COR_PRINCIPAL), size_hint=(None, None), size=(400, 200))
             popup_erro.open()
 
-if __name__ == '__main__':
+    def mostrar_popup_seno(self, instance):
+        content = BoxLayout(orientation='vertical', padding=10, spacing=5)
+        content.add_widget(Label(text="Ângulo (em graus):", color=COR_PRINCIPAL))
+        self.angulo_input = TextInput(
+            multiline=False, background_color=COR_FUNDO, height=50)
+        content.add_widget(self.angulo_input)
+
+        button_calcular = Button(
+            text="Calcular", background_color=COR_PRINCIPAL, size_hint_y=None, height=50)
+        button_calcular.bind(on_press=self.calcular_seno_callback)
+
+        content.add_widget(button_calcular)
+
+        popup = Popup(title='Calcular Seno',
+                      content=content, size_hint=(None, None), size=(400, 350))
+        popup.open()
+
+    def calcular_seno_callback(self, instance):
+        angulo = self.angulo_input.text
+        try:
+            angulo = float(angulo)
+            resultado = math.sin(math.radians(angulo))  
+            popup_resultado = Popup(title='Resultado', content=Label(
+                text=f'O seno de {angulo}° é {resultado:.4f}', color=COR_PRINCIPAL), size_hint=(None, None), size=(400, 200))
+            popup_resultado.open()
+        except ValueError:
+            popup_erro = Popup(title='Erro', content=Label(
+                text='Por favor, insira um ângulo válido.', color=COR_PRINCIPAL), size_hint=(None, None), size=(400, 200))
+            popup_erro.open()
+
+    def mostrar_popup_cosseno(self, instance):
+        content = BoxLayout(orientation='vertical', padding=10, spacing=5)
+        content.add_widget(Label(text="Ângulo (em graus):", color=COR_PRINCIPAL))
+        self.angulo_input = TextInput(
+            multiline=False, background_color=COR_FUNDO, height=50)
+        content.add_widget(self.angulo_input)
+
+        button_calcular = Button(
+            text="Calcular", background_color=COR_PRINCIPAL, size_hint_y=None, height=50)
+        button_calcular.bind(on_press=self.calcular_cosseno_callback)
+
+        content.add_widget(button_calcular)
+
+        popup = Popup(title='Calcular Cosseno',
+                      content=content, size_hint=(None, None), size=(400, 350))
+        popup.open()
+
+    def calcular_cosseno_callback(self, instance):
+        angulo = self.angulo_input.text
+        try:
+            angulo = float(angulo)
+            resultado = math.cos(math.radians(angulo)) 
+            popup_resultado = Popup(title='Resultado', content=Label(
+                text=f'O cosseno de {angulo}° é {resultado:.4f}', color=COR_PRINCIPAL), size_hint=(None, None), size=(400, 200))
+            popup_resultado.open()
+        except ValueError:
+            popup_erro = Popup(title='Erro', content=Label(
+                text='Por favor, insira um ângulo válido.', color=COR_PRINCIPAL), size_hint=(None, None), size=(400, 200))
+            popup_erro.open()
+
+    def mostrar_popup_exponencial(self, instance):
+        content = BoxLayout(orientation='vertical', padding=10, spacing=5)
+        content.add_widget(Label(text="Base:", color=COR_PRINCIPAL))
+        self.base_input = TextInput(
+            multiline=False, background_color=COR_FUNDO, height=50)
+        content.add_widget(self.base_input)
+        content.add_widget(Label(text="Expoente:", color=COR_PRINCIPAL))
+        self.expoente_input = TextInput(
+            multiline=False, background_color=COR_FUNDO, height=50)
+        content.add_widget(self.expoente_input)
+
+        button_calcular = Button(
+            text="Calcular", background_color=COR_PRINCIPAL, size_hint_y=None, height=50)
+        button_calcular.bind(on_press=self.calcular_exponencial_callback)
+
+        content.add_widget(button_calcular)
+
+        popup = Popup(title='Calcular Exponencial',
+                      content=content, size_hint=(None, None), size=(400, 350))
+        popup.open()
+
+    def calcular_exponencial_callback(self, instance):
+        base = self.base_input.text
+        expoente = self.expoente_input.text
+        try:
+            base = float(base)
+            expoente = float(expoente)
+            resultado = math.pow(base, expoente)
+            popup_resultado = Popup(title='Resultado', content=Label(
+                text=f'{base}^{expoente} = {resultado:.2f}', color=COR_PRINCIPAL), size_hint=(None, None), size=(400, 200))
+            popup_resultado.open()
+        except ValueError:
+            popup_erro = Popup(title='Erro', content=Label(
+                text='Por favor, insira valores numéricos.', color=COR_PRINCIPAL), size_hint=(None, None), size=(400, 200))
+            popup_erro.open()
+
+    def mostrar_popup_log_natural(self, instance):
+        content = BoxLayout(orientation='vertical', padding=10, spacing=5)
+        content.add_widget(Label(text="Número:", color=COR_PRINCIPAL))
+        self.num_input = TextInput(
+            multiline=False, background_color=COR_FUNDO, height=50)
+        content.add_widget(self.num_input)
+
+        button_calcular = Button(
+            text="Calcular", background_color=COR_PRINCIPAL, size_hint_y=None, height=50)
+        button_calcular.bind(on_press=self.calcular_log_natural_callback)
+
+        content.add_widget(button_calcular)
+
+        popup = Popup(title='Calcular Log Natural',
+                    content=content, size_hint=(None, None), size=(400, 230))
+        popup.open()
+
+    def calcular_log_natural_callback(self, instance):
+        num = self.num_input.text
+        try:
+            num = float(num)
+            resultado = math.log(num)
+            popup_resultado = Popup(title='Resultado', content=Label(
+                text=f'O logaritmo natural de {num} é {resultado:.2f}', color=COR_PRINCIPAL), size_hint=(None, None), size=(400, 200))
+            popup_resultado.open()
+        except ValueError:
+            popup_erro = Popup(title='Erro', content=Label(
+                text='Por favor, insira um número válido.', color=COR_PRINCIPAL), size_hint=(None, None), size=(400, 200))
+            popup_erro.open()
+
+    def mostrar_popup_equacao_movimento(self, instance):
+        content = BoxLayout(orientation='vertical', padding=10, spacing=5)
+        content.add_widget(Label(text="Velocidade Inicial (m/s):", color=COR_PRINCIPAL))
+        self.velocidade_inicial_input = TextInput(
+            multiline=False, background_color=COR_FUNDO, height=50)
+        content.add_widget(self.velocidade_inicial_input)
+        content.add_widget(Label(text="Aceleração (m/s^2):", color=COR_PRINCIPAL))
+        self.aceleracao_input = TextInput(
+            multiline=False, background_color=COR_FUNDO, height=50)
+        content.add_widget(self.aceleracao_input)
+        content.add_widget(Label(text="Tempo (s):", color=COR_PRINCIPAL))
+        self.tempo_input = TextInput(
+            multiline=False, background_color=COR_FUNDO, height=50)
+        content.add_widget(self.tempo_input)
+
+        button_calcular = Button(
+            text="Calcular", background_color=COR_PRINCIPAL, size_hint_y=None, height=50)
+        button_calcular.bind(on_press=self.calcular_equacao_movimento_callback)
+
+        content.add_widget(button_calcular)
+
+        popup = Popup(title='Calcular Equação de Movimento',
+                      content=content, size_hint=(None, None), size=(400, 350))
+        popup.open()
+
+    def calcular_equacao_movimento_callback(self, instance):
+        velocidade_inicial = self.velocidade_inicial_input.text
+        aceleracao = self.aceleracao_input.text
+        tempo = self.tempo_input.text
+        resultado = calcular_equacao_movimento(
+            velocidade_inicial, aceleracao, tempo)
+        if resultado is not None:
+            popup_resultado = Popup(title='Resultado', content=Label(
+                text=f'A posição final é {resultado:.2f} metros', color=COR_PRINCIPAL), size_hint=(None, None), size=(400, 200))
+            popup_resultado.open()
+        else:
+            popup_erro = Popup(title='Erro', content=Label(
+                text='Por favor, insira valores numéricos.', color=COR_PRINCIPAL), size_hint=(None, None), size=(400, 200))
+            popup_erro.open()
+
+
+
+if __name__ == "__main__":
     CalculadoraApp().run()
